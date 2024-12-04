3f45482e2c761ec867e98073e0e3552759d9a575
Jhennifer Bueno <84140469+JhenniferBueno@users.noreply.github.com>
2024-12-03 21:52:38 -0300

    Add files via upload

--- node-dezembro/ola.js
+++ node-dezembro/ola.js
@@ -1,22 +1,2 @@
 console.log("oi turma");
 
-//imprimir na tel anumeros de 1 a 10
-
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
-}
-
-for (let i = 10; i >= 1; i--) {
-  console.log(i);
-}
-
-
-const nome = "edir"; // null
-//nome = "paulo"; // string
-
-//let nome ="edir";//
-
-
-let texto = "que dia ensolarado";
-
-console.log(texto);
